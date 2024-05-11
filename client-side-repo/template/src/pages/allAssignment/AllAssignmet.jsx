2438c453a7064f598a205dc757866104a192b450
unknown <redoyhasan4585@gmail.com>
2024-05-11 13:08:26 +0400

    added structure of assignment page

--- client-side-repo/template/src/pages/allAssignment/AllAssignmet.jsx
+++ client-side-repo/template/src/pages/allAssignment/AllAssignmet.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import useAxiosUrl from "../../hooks/useAxiosUrl";
 import { Link } from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
+import { TiDelete } from "react-icons/ti";
+
+
 
 const AllAssignmet = () => {
   const [assignments, setAssignments] = useState([]);
@@ -51,9 +55,9 @@ const AllAssignmet = () => {
     <div className=" grid grid-cols-1 md:grid-cols-2 gap-5 w-[98%] mx-auto mt-20">
       {
         assignments?.map(singleData=><div key={singleData._id} className="card  bg-base-100 shadow-xl flex-col  p-2 lg:p-3 border border-lime-300">
-        <figure className="w-[50%] mx-auto h-52 rounded-lg "><img className=" h-full w-full" src={singleData?.photoURL} alt="banner image"/></figure>
+        <figure className="w-[80%] mx-auto h-52 rounded-lg "><img className=" h-full w-full" src={singleData?.photoURL} alt="banner image"/></figure>
         <div className="card-body w-full  ">
-          <h2 className="card-title text-xl md:text-2xl font-bold hover:underline">{singleData?.title}</h2>
+          <h2 className="card-title text-xl lg:text-2xl font-bold hover:underline">{singleData?.title}</h2>
           <div>
             {singleData?.description.length>100?
             <p>{singleData?.description.slice(0,100)}...<Link className=" font-bold text-blue-600">Read More.</Link> </p>
@@ -61,7 +65,18 @@ const AllAssignmet = () => {
             <p>{singleData?.description}</p>
           }
           </div>
-          
+          <div className="divider"></div>
+          <div className=" flex justify-between ">
+            <h2 className=" text-base lg:text-xl font-semibold  ">Difficulty Level: <span className=" text-cyan-500 font-bold">{singleData.levels}</span></h2>
+            <h2 className=" text-base lg:text-xl font-semibold  ">Marks: <span className=" text-orange-500 font-bold">{singleData.marks}</span></h2>
+          </div>
+          <div className=" flex justify-between my-5 ">
+          <button className="btn btn-success text-white ">Update <span className=" text-xl"><FaEdit></FaEdit></span></button>
+         
+          <button className="btn btn-error text-white ">Delete <span className=" text-3xl"><TiDelete></TiDelete></span></button>
+         
+          </div>
+          <button className=" btn btn-block text-xl font-bold my-5 btn-accent btn-outline">View Assignment</button>
          
         </div>
       </div>)
